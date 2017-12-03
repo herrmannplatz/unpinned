bbff9de03f7917edb69dd1a55ee787794b641099
Lipis <lipiridis@gmail.com>
2017-12-03 11:48:54 +0100

    chore(cli): Better output format (#5)

--- cli.js
+++ cli.js
@@ -11,14 +11,28 @@ const absolutePath = path.endsWith('package.json')
   ? resolve(path)
   : resolve(path, 'package.json')
 
-console.log(`Looking for package.json in path ${absolutePath}`)
 const unpinnedDependencies = unpinned(absolutePath)
 
+function prettyPrint (dependencies) {
+  let longest = 0
+  for (const name in Object.assign({}, dependencies.dependencies, dependencies.devDependencies)) {
+    if (name.length > longest) {
+      longest = name.length
+    }
+  }
+
+  for (const key of ['dependencies', 'devDependencies']) {
+    for (const name in dependencies[key]) {
+      console.log(' ', name, ' '.repeat(longest - name.length), dependencies[key][name])
+    }
+  }
+}
+
 if (unpinnedDependencies) {
-  console.log('Found unpinned dependencies')
-  console.log(JSON.stringify(unpinnedDependencies, null, 2))
+  console.log('\nFound unpinned dependencies\n')
+  prettyPrint(unpinnedDependencies)
   process.exit(1)
 } else {
-  console.log('Couldnt find any unpinned depedency')
+  console.log("\nCouldn't find any unpinned dependency")
   process.exit()
 }
